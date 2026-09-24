4123475906ecb5e1eb32d33cccee365049c12d4f
ManuProDev <74546221+ManuProDev@users.noreply.github.com>
2026-09-24 11:21:54 +0200

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("scroll", () => {
-  const scrollTop = window.scrollY;
-  const docHeight = document.body.scrollHeight - window.innerHeight;
-  const progress = (scrollTop / docHeight) * 100;
-  document.getElementById("scrollProgress").style.width = progress + "%";
-});
